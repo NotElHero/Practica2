f45ff7e65fc2866fac0fad8bbc4fb26d64c1c514
admin <javiervg01@gmail.com>
2022-10-17 21:28:43 +0200

    v3

--- app/src/main/java/com/example/practica2/MainActivity.java
+++ app/src/main/java/com/example/practica2/MainActivity.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
 
+    ImageButton avatar1, avatar2, avatar3, avatar4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +22,15 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         genero.setOnItemSelectedListener(this);
         Spinner comunidad = findViewById(R.id.spinnerComu);
         comunidad.setOnItemSelectedListener(this);
+
+        avatar1 = findViewById(R.id.avatar1);
+        avatar1.setOnClickListener(this);
+        avatar2 = findViewById(R.id.avatar2);
+        avatar2.setOnClickListener(this);
+        avatar3 = findViewById(R.id.avatar3);
+        avatar3.setOnClickListener(this);
+        avatar4 = findViewById(R.id.avatar4);
+        avatar4.setOnClickListener(this);
 
     }
 
@@ -31,5 +42,35 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.avatar1:
+                avatar1.getBackground().setAlpha(255);
+                avatar2.getBackground().setAlpha(128);
+                avatar3.getBackground().setAlpha(128);
+                avatar4.getBackground().setAlpha(128);
+                break;
+            case R.id.avatar2:
+                avatar1.getBackground().setAlpha(128);
+                avatar2.getBackground().setAlpha(255);
+                avatar3.getBackground().setAlpha(128);
+                avatar4.getBackground().setAlpha(128);
+                break;
+            case R.id.avatar3:
+                avatar1.getBackground().setAlpha(128);
+                avatar2.getBackground().setAlpha(128);
+                avatar3.getBackground().setAlpha(255);
+                avatar4.getBackground().setAlpha(128);
+                break;
+            case R.id.avatar4:
+                avatar1.getBackground().setAlpha(128);
+                avatar2.getBackground().setAlpha(128);
+                avatar3.getBackground().setAlpha(128);
+                avatar4.getBackground().setAlpha(255);
+                break;
+        }
     }
 }
